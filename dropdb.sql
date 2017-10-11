008d4b71b8a597e4a64e7a138e8d5ee625525830
Naphat Wetayaphong <naphat.wetayaphong@allianz.com>
2017-10-11 17:58:39 +0700

    added drop db script

---
+++ dropdb.sql
@@ -0,0 +1,28 @@
+drop table `cloudstock`.`transfer_item`;
+drop table `cloudstock`.`transfer`;
+drop table `cloudstock`.`transaction_item`;
+drop table `cloudstock`.`transaction`;
+drop table `cloudstock`.`stock`;
+drop table `cloudstock`.`attribute_item`;
+drop table `cloudstock`.`attribute`;
+drop table `cloudstock`.`app_config`;
+drop table `cloudstock`.`bill_item`;
+drop table `cloudstock`.`bill`;
+drop table `cloudstock`.`sell`;
+drop table `cloudstock`.`sku`;
+drop table `cloudstock`.`branch`;
+drop table `cloudstock`.`product`;
+drop table `cloudstock`.`product_type`;
+drop table `cloudstock`.`jhi_user_authority`;
+drop table `cloudstock`.`jhi_user`;
+drop table `cloudstock`.`jhi_social_user_connection`;
+drop table `cloudstock`.`jhi_persistent_audit_evt_data`;
+drop table `cloudstock`.`jhi_persistent_audit_event`;
+drop table `cloudstock`.`jhi_authority`;
+drop table `cloudstock`.`databasechangeloglock`;
+drop table `cloudstock`.`databasechangelog`;
+
+
+
+
+
